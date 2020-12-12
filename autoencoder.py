2d40c72c0eabf7030f146aee9155df65a092685c
damik3 <d1mike@hotmail.com>
2020-12-12 18:14:16 +0200

    autoencoder: completed

--- autoencoder.py
+++ autoencoder.py
@@ -9,11 +9,12 @@ import tensorflow as tf
 from keras import models, layers, optimizers, losses, metrics
 from Image import Image
 
-
+AUTOENCODER_LATENT_VECTOR_SIZE = 10
+AUTOENCODER_NORM_FACTOR = 255
 
 
 def usage():
-   print('autoencoder.py  –d <dataset>  -dupto <optional int>  -o <output file>')
+   print('autoencoder.py  –d <dataset>  -dupto <optional int>  -q <query file>  -qupto <optional int>  -o <output file>')
 
 
 
@@ -21,10 +22,12 @@ def usage():
 def getCommandLineArgs(argv):
 
    inputFile=None
-   outputFile=None
    readUpto=None
+   queryFile=None
+   queryUpto=None
+   outputFile=None
 
-   if (len(argv) != 4) and (len(argv) != 6):
+   if (len(argv) != 6) and (len(argv) !=8) and (len(argv) != 10):
       print("Wrong number of parameters!")
       usage()
       sys.exit()
@@ -38,6 +41,12 @@ def getCommandLineArgs(argv):
       elif argv[i] == "-dupto":
          i += 1
          readUpto = int(argv[i])
+      elif argv[i] == "-q":
+         i += 1
+         queryFile = argv[i]
+      elif argv[i] == "-qupto":
+         i += 1
+         queryUpto = int(argv[i])
       elif argv[i] == "-o":
          i += 1
          outputFile = argv[i]
@@ -47,11 +56,11 @@ def getCommandLineArgs(argv):
       
       i += 1
 
-   if inputFile is None:
+   if inputFile is None or queryFile is None or outputFile is None:
       usage()
       sys.exit()
 
-   return (inputFile, readUpto, outputFile)
+   return (inputFile, readUpto, queryFile, queryUpto, outputFile)
 
 
 
@@ -62,14 +71,11 @@ def getExecParams():
    numConvLay = int(input("Please, give number of convolutional layers... "))
    
    sizeConvFil = []
-
    for i in range(numConvLay):
       sizeConvFil.append(int(input("Please, give size of filter " + str(i) + "... ")))
 
    numConvFilPerLay = int(input("Please, give number of convolutional filters per layer... "))
-
    epochs = int(input("Please, give number of epochs... "))
-
    batchSize = int(input("Please, give number of batch size... "))
 
    return (numConvLay, sizeConvFil, numConvFilPerLay, epochs, batchSize)
@@ -95,6 +101,20 @@ def getIdxHeaders(f):
 
 
 
+def writeIdx(f, magicnumber, numimages, numrows, numcols, array):
+
+   f.write((magicnumber).to_bytes(4, byteorder="big"))
+   f.write((numimages).to_bytes(4, byteorder="big"))
+   f.write((numrows).to_bytes(4, byteorder="big"))
+   f.write((numcols).to_bytes(4, byteorder="big"))
+
+   for row in array:
+      for coor in row:
+         f.write((coor).to_bytes(1, byteorder="big"))
+   
+
+
+
 def encoder(input_img, numConvLay, sizeConvFil, numConvFilPerLay):
    # input_img = 28 x 28 x 1
 
@@ -109,7 +129,7 @@ def encoder(input_img, numConvLay, sizeConvFil, numConvFilPerLay):
       ret = layers.MaxPooling2D(pool_size=(2, 2))(ret)
 
    ret = layers.Flatten()(ret)
-   ret = layers.Dense(10, activation='relu')(ret)
+   ret = layers.Dense(AUTOENCODER_LATENT_VECTOR_SIZE, activation='relu')(ret)
 
    return ret
 
@@ -143,7 +163,7 @@ def decoder(latent_vector, numConvLay, sizeConvFil, numConvFilPerLay, img_numrow
 def main(argv):
 
    # Get command line arguements
-   inputFile, readUpTo, outputFile = getCommandLineArgs(argv)
+   inputFile, readUpto, queryFile, queryUpto, outputFile = getCommandLineArgs(argv)
 
    # Get hyperparameters 
    numConvLay, sizeConvFil, numConvFilPerLay, epochs, batchSize = getExecParams()
@@ -162,23 +182,53 @@ def main(argv):
    #Get IDX Headers
    magicnumber, numofimages, numrows, numcols = getIdxHeaders(f)
 
-   if (readUpTo is None) or (readUpTo > numofimages):
-      readUpTo = numofimages
+   if (readUpto is None) or (readUpto > numofimages):
+      readUpto = numofimages
 
    img = Image(numrows, numcols)
    images = []
-   train_images = np.zeros([readUpTo, numrows, numcols])
+   train_images = np.zeros([readUpto, numrows, numcols])
 
-   print("Scanning images...")
+   print("Scanning input images...")
    
-   for i in range(readUpTo):
+   for i in range(readUpto):
       img.scan(f, i)
       images.append(copy.deepcopy(img))
       train_images[i] = copy.deepcopy(img.pixels)
 
-   print(readUpTo, " images scanned!")
+   f.close()
+
+   print(readUpto, "images scanned!")
 
 
+
+   #
+   # Read query file
+   #
+
+   f = open(queryFile, "rb")
+
+   #Get IDX Headers
+   qmagicnumber, qnumofimages, qnumrows, qnumcols = getIdxHeaders(f)
+
+   if (queryUpto is None) or (queryUpto > qnumofimages):
+      queryUpto = qnumofimages
+
+   img = Image(qnumrows, qnumcols)
+   queries = []
+   query_images = np.zeros([queryUpto, qnumrows, qnumcols])
+
+   print("Scanning query images...")
+
+   for i in range(queryUpto):
+      img.scan(f, i)
+      queries.append(copy.deepcopy(img))
+      query_images[i] = copy.deepcopy(img.pixels)
+
+   f.close()
+
+   print(queryUpto, "images scanned!")
+   
 
    # Split training set
 
@@ -200,7 +250,7 @@ def main(argv):
    autoencoder.compile(loss='mean_squared_error', optimizer= optimizers.RMSprop())
 
 
-
+   
    # Print summary
    print(autoencoder.summary())
 
@@ -213,27 +263,91 @@ def main(argv):
    encoder_model = models.Model(input_img, encoder(input_img, numConvLay, sizeConvFil, numConvFilPerLay))
    
    # Load weights from autoencoder
-   trained_layers_num = numConvLay*(3*numConvFilPerLay + 1) + 1 + 2
+   # trained_layers_num = numConvLay*(3*numConvFilPerLay + 1) + 1 + 2
+   trained_layers_num = len(encoder_model.layers)
    for l1, l2 in zip(encoder_model.layers[0:trained_layers_num], autoencoder.layers[0:trained_layers_num]):
         l1.set_weights(l2.get_weights())
    
    # Compile encoder_model
    encoder_model.compile(loss=losses.categorical_crossentropy, optimizer=optimizers.Adam(),metrics=['accuracy'])
 
+
+   
    # Predict
-   predictions = encoder_model.predict(train_images)
+   predictions = encoder_model.predict(query_images)
+   
+
+
+   #
+   # Normalize predictions in [0, 255]
+   #
+
+   # predictions = [[0.1, 500.6, 1000.0], [100.8, 250.1, 750.6], [150.0, 500.2, 100.1]]
+
+   # Find min and max
+   norm_max = max([max(p) for p in predictions])
+   
+   # Edge case, not supposed to happen
+   if norm_max == 0:
+      norm_max = 1
+
+   norm_min = min([min(p) for p in predictions])
+   
+   print("\n\nmax:", norm_max, "min:", norm_min, end="\n\n")
+
+   # Normalize
+   normalized_predictions = [[] for p in predictions]
+   for i in range(len(predictions)):
+      normalized_predictions[i] = [int(round((coord/norm_max) * AUTOENCODER_NORM_FACTOR)) for coord in predictions[i]]
+
+   
+   # for i in range(len(predictions)):
+      # print("predictions[i]:", predictions[i], "\n normalized_predictions[i]:", normalized_predictions[i], end='\n\n')
+
+
+
+   #
+   # Write predictions to output file
+   #
+
+   f = open(outputFile, "wb")
+   writeIdx(f, 666, queryUpto, 1, AUTOENCODER_LATENT_VECTOR_SIZE, normalized_predictions)
+   f.close()
+
+
+
+   #
+   # Check first n vectors
+   #
+   n = 10
+
+   f = open(outputFile, "rb")
+   omagicnumber, onumofimages, onumrows, onumcols = getIdxHeaders(f)
+
+   for i in range(n):
+      for j in range(onumrows*onumcols):
+         val = f.read(1)
+         val = int.from_bytes(val, "big")
+         print(val, end=' ')
+      print()
+
+   f.close()
 
    # Print some predictions
-   print("Printing 10 first images with their latent vectors...")
+
+   print("Printing 10 first images with their latent vectors and normalized predictions...")
+
    for i in range(10):
       images[i].print()
-      print(predictions[i])
+      print(predictions[i])   
+      print(normalized_predictions[i])
+
 
 
    # Graph loss on train and loss on validation set
-
+   
    ans = input("Show graph loss on training and validation set? (y/n) ... ")
-
+   
    if ans == 'y':
       plt.plot(history.history['loss'])
       plt.plot(history.history['val_loss'])
