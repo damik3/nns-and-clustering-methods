76063f0bdcf730552e0a3ef8c9959c13ca884e71
damik3 <d1mike@hotmail.com>
2020-12-30 22:47:21 +0200

    python/dist: imple Manhattan, cluster_sum, cluster_pos and reworked some stuff

--- python/dist.py
+++ python/dist.py
@@ -1,66 +1,193 @@
 import sys
 import math
-import numpy as np
+import unittest
 
 from Image import Image
 
+
+
 def is_perfect_square(s):
     r = math.sqrt(s)
-    return r == np.floor(r)
+    return r == math.floor(r)
 
-def EmdDistance(img1, img2, num_clusters):
 
-    p1 = img1.pixels
-    p2 = img2.pixels
+
+
+def manhattan_dist(v1, v2):
+
+    if len(v1) != len(v2):
+        raise Exception("dist.manhattan_dist: Unequal vector sizes!")
+
+    ret = 0
+    for i in range(len(v1)):
+        ret += abs(v1[i] - v2[i])
+    
+    return ret
+
+
+
+
+def euclidean_dist(v1,v2):
+
+    if len(v1) != len(v2):
+        raise Exception("dist.euclidean_dist: Unequal vector sizes!")
+
+    ret = 0
+    for i in range(len(v1)):
+            ret += pow(v1[i] - v2[i], 2)
+    
+    r = math.sqrt(ret)
+    return r
+
+
+
+
+def dist(v1, v2, method, n):
+    if method == "Manhattan":
+        return manhattan_dist(v1, v2)
+    if method == "Euclidean":
+        return euclidean_dist(v1, v2)
+    elif method == "EMD":
+        return emd_dist(v1, v2, n)
+    else:
+        raise Exception("Dist: Invalid method!")
+
+
+
+
+def cluster_pos(v, n, r):
+    
+    num_cols = math.sqrt(len(v))
+    cluster_size = r*r
+    clusters_per_row = num_cols / r
+    num_clusters = len(v) / cluster_size
+
+    if (n < 0) or (n >= num_clusters):
+        raise Exception("Dist.cluster_pos: Invalid cluster number!")
+    
+    return (r * (n % clusters_per_row), r * (n // clusters_per_row))
+
+
+
+
+def cluster_sum(v, cluster_pos, r):
+    
+    assert(is_perfect_square(len(v)))
+
+    ret = 0
+    count = r*r
+    num_cols = int(math.sqrt(len(v)))
+    num_rows = num_cols
+
+    if (cluster_pos[0] < 0) or (cluster_pos[0] >= num_cols) or (cluster_pos[1] < 0) or (cluster_pos[1] >= num_rows):
+        raise Exception("Dist.cluster_sum: Invalid cluster position!")
+
+    # v is a 1d vector
+    # but represents 2d array
+    # v starts counting from upper left corner of the 2d array, that is its (0, 0) position
+    # for some reason, we decided that for cluster(i, j) we start counting for bottom left corner
+    # thus the (num_rows - j - 1) coefficient
+
+    j = cluster_pos[1]
+
+    while 0 < count:
+        i = cluster_pos[0]
+        while i < cluster_pos[0] + r:
+            ret += v[ (num_rows - j - 1) * num_cols + i ]
+            count -= 1
+            i += 1
+        j = (j+1) % r + cluster_pos[1]
+
+    return ret
+
+
+
+
+def emd_dist(v1, v2, num_clusters):
 
     if num_clusters == 0:
-        print("dist.EmdDistance error : Divided by zero !")
-        sys.exit()
+        raise Exception("dist.emd_dist error : Divided by zero !")
 
+    if len(v1) != len(v2):
+        raise Exception("dist.emd_dist error : Invalid vector sizes")
 
-    if p1.size != p2.size:
-        print("dist.EmdDistance error : Invalid vector sizes")
-        sys.exit()
-
-    cluster_size = p1.size / num_clusters
+    cluster_size = len(v1) / num_clusters
     if is_perfect_square(cluster_size) == 0:
-        print("dist.EmdDistance error : cluster size not a perfect square")
-        sys.exit()
+        raise Exception("dist.emd_dist error : cluster size not a perfect square")
 
     r = math.sqrt(cluster_size)
 
-    sum1 = np.sum(p1)
-    sum2 = np.sum(p2)
+    sum1 = sum(v1)
+    sum2 = sum(v2)
 
-    print(sum1)
+
+
+
+
+
+class Test(unittest.TestCase):
     
-def EuclideanDistance(img1,img2):
+    @classmethod
+    def setUpClass(self):
+        self.v_1x1_1 = [1]
+        self.v_2x2_1 = [1, 2, 3, 4]
+        self.v_2x2_2 = [3, 4, 1, 2]
+        self.v_4x4_1 = [1, 2, 5, 6, 3, 4, 7, 8, 9, 10, 13, 14, 11, 12, 15, 16]
+        self.v_4x4_2 = [11, 12, 15, 16, 9, 10, 13, 14, 3, 4, 7, 8, 1, 2, 5, 6]
+        self.v_6x6_1 = [1, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 1, 3, 4, 5, 6, 1, 2, 4, 5, 6, 1, 2, 3, 5, 6, 1, 2, 3, 4, 6, 1, 2, 3, 4, 5]
+        self.v_6x6_2 = [6, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 6, 4, 3, 2, 1, 6, 5, 3, 2, 1, 6, 5, 4, 2, 1, 6, 5, 4, 3, 1, 6, 5, 4, 3, 2]
 
-    p1 = img1.pixels
-    p2 = img2.pixels
-    
-    if p1.size != p2.size:
-        print("dist.ManhattanDistance error : Invalid vector sizes")
-        sys.exit()
+
+    def test_is_perfect_square(self):
+        self.assertTrue(is_perfect_square(1))
+        self.assertTrue(is_perfect_square(4))
+        self.assertTrue(is_perfect_square(9))
+        self.assertTrue(is_perfect_square(196))
+
+        self.assertFalse(is_perfect_square(2))
+        self.assertFalse(is_perfect_square(3))
+        self.assertFalse(is_perfect_square(8))
+        self.assertFalse(is_perfect_square(195))
 
 
-    ret = 0;
-    for i in range(img1.numrows):
-        for j in range(img1.numcols):
-            ret += pow(p1[i][j] - p2[i][j], 2)
+    def test_manhattan(self):
+        self.assertEqual(manhattan_dist(self.v_1x1_1, self.v_1x1_1), 0)
+        self.assertEqual(manhattan_dist(self.v_2x2_1, self.v_2x2_2), 8)
+        self.assertEqual(manhattan_dist(self.v_4x4_1, self.v_4x4_2), 128)
+        
+        self.assertRaises(Exception, lambda: manhattan_dist(self.v_2x2_2, self.v_4x4_1))
 
-    r = math.sqrt(ret)
 
-    return r
+    def test_euclidean(self):
+        self.assertEqual(euclidean_dist(self.v_1x1_1, self.v_1x1_1), 0)
+        self.assertEqual(euclidean_dist(self.v_2x2_1, self.v_2x2_2), math.sqrt(16))
+        self.assertEqual(euclidean_dist(self.v_4x4_1, self.v_4x4_2), math.sqrt(1088))
+        
+        self.assertRaises(Exception, lambda: euclidean_dist(self.v_1x1_1, self.v_2x2_1))
 
-def dist(img1, img2, method, n):
-    if method == "Euclidean":
-        ret = EuclideanDistance(img1, img2)
-        return ret
-    elif method == "EMD":
-        ret = EmdDistance(img1, img2, n)
-        return
-    else:
-        print("Invalid method")
-        sys.exit()
 
+    def test_cluster_pos(self):
+        self.assertEqual(cluster_pos(self.v_6x6_1, 3, 2), (0, 2))
+        self.assertEqual(cluster_pos(self.v_6x6_1, 0, 2), (0, 0))
+        self.assertEqual(cluster_pos(self.v_6x6_1, 8, 2), (4, 4))
+
+        self.assertEqual(cluster_pos(self.v_6x6_2, 2, 3), (0, 3))
+        self.assertEqual(cluster_pos(self.v_6x6_2, 0, 3), (0, 0))
+        self.assertEqual(cluster_pos(self.v_6x6_2, 3, 3), (3, 3))
+
+        self.assertRaises(Exception, lambda: cluster_pos(self.v_6x6_1, 9, 2))
+        self.assertRaises(Exception, lambda: cluster_pos(self.v_6x6_1,-1, 2))
+        self.assertRaises(Exception, lambda: cluster_pos(self.v_6x6_1, 4, 3))
+        self.assertRaises(Exception, lambda: cluster_pos(self.v_6x6_1,-1, 3))
+
+
+    def test_cluster_sum(self):
+        self.assertEqual(cluster_sum(self.v_6x6_2, (0, 2), 2), 12)
+        self.assertEqual(cluster_sum(self.v_6x6_2, (0, 0), 2), 10)
+        self.assertEqual(cluster_sum(self.v_6x6_2, (4, 4), 2), 10)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
